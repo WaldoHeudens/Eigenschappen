b314d2d0b716e8d33528e72bd9bfa369209ec73d
Waldo Heudens <waldo@heudens.be>
2023-09-25 14:16:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Eigenschappen;
+
+Console.WriteLine("Geef een aantal studenten in");
+Console.WriteLine("   Een lege naam beëindigt de loop");
+
+string naam, opleiding;
+List<Student> studenten = new List<Student>();
+Student.Volgnummer = 1825;
+
+do
+{
+    Console.Write("Geef de naam van de student:");
+    naam = Console.ReadLine();
+    if (naam != "")
+    {
+        Console.Write("Geef de naam van de opleiding:");
+        opleiding = Console.ReadLine();
+        studenten.Add(new Student(naam, opleiding));
+    }
+}
+while (naam != "");
+
+foreach (Student student in studenten)
+    Console.WriteLine(student.ToString());
---
+++ Student.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Eigenschappen
+{
+    internal class Student
+    {
+        // Definieer een statische eigenschap "Volgnummer" met expliciete get en set accessors
+        public static int Volgnummer { get { volgnummer++; return volgnummer; } set { volgnummer = value; } }
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string Opleiding { get; set; }
+
+        private static int volgnummer = 0;
+
+
+        public Student()
+        {
+            Id = Volgnummer;
+        }
+
+        public Student(string naam, string opleiding)
+        {
+            Id = Volgnummer;  // Ken het volgende "volgnummer" toe aan Id
+            Naam = naam;
+            Opleiding = opleiding;
+        }
+
+        public override string ToString()
+        {
+
+            return "Id: " + Id + "; Naam: " + Naam + ";  Opleiding: " + Opleiding;
+        }
+    }
+}
